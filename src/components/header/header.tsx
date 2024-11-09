01b5ecc4ea72dc9fdec77cbd2b0c3734a1a581c5
Vinicius Duarte <viniciusduarte@outlook.com>
2024-11-09 14:36:58 +0000

    Atualizar o header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -43,8 +43,7 @@ export const Header = ({ isComplete = true }) => {
           <>
             <div className={styles.subtitle}>
               <h1>
-                Aderir Pacotes de Internet {showNOSName && "N​O​S "}Fibra ou
-                Satélite ao melhor preço de sempre
+                Aderir Pacotes de Internet {showNOSName && "N​O​S "}Fibra ao melhor preço de sempre
               </h1>
             </div>
             <div className={styles.details}>
