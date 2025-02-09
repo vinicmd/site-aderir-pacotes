513b60183fe9e5d56e1ca42242ac15b603169514
Vinicius Duarte <viniciusduarte@outlook.com>
2025-02-09 19:16:17 +0000

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -18,7 +18,7 @@ export const Header = ({ isComplete = true }) => {
         <div className={styles.title}>
           <Link href="/">
             <Image
-              src="/assets/logo.png"
+              src="/assets/logo.svg"
               className={styles.logo}
               alt="Logo"
               width={185}
